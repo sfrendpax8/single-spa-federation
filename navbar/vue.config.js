b859fe4bb20a59761deace9ca0e74b18b81968fa
Steve Frend <69208557+sfrendpax8@users.noreply.github.com>
2022-02-02 16:16:18 -0700

    navbar works with both vue versions present

---
+++ navbar/vue.config.js
@@ -0,0 +1,20 @@
+const port = 8083;
+const projectName = 'navbar'; // can probably retrieve through script or packageJson property
+
+module.exports = {
+  configureWebpack: {
+    output: {
+      // filename: `js/${projectName}.js`,
+      devtoolNamespace: projectName,
+    },
+    externals: [],
+  },
+
+  devServer: {
+    https: false,
+    host: 'localhost',
+    port,
+  },
+  filenameHashing: false,
+};
+
--- shell/src/root-config.js
+++ shell/src/root-config.js
@@ -1,27 +1,27 @@
 import { registerApplication, start } from "single-spa";
 
-// registerApplication({
-//   name: "vue2-main",
-//   app: () => {
-//     return System.import("vue2-main");
-//   },
-//   activeWhen: "/",
-// });
+registerApplication({
+  name: "vue2-main",
+  app: () => {
+    return System.import("vue2-main");
+  },
+  activeWhen: "/vue2-main",
+});
 
 registerApplication({
   name: "vue3app",
   app: () => {
     return System.import("vue3app");
   },
-  activeWhen: "/",
+  activeWhen: "/vue3app",
 });
 
-// registerApplication({
-//   name: "navbar",
-//   app: async () => {
-//     return System.import("navbar");
-//   },
-//   activeWhen: "/",
-// });
+registerApplication({
+  name: "navbar",
+  app: async () => {
+    return System.import("navbar");
+  },
+  activeWhen: "/",
+});
 
 start();
