cec1ad24250a861083f46e332b75db88aaf5df8a
Steve Frend <69208557+sfrendpax8@users.noreply.github.com>
2022-02-04 11:46:24 -0700

    upgrade vue3app to webpack 5 - works

--- vue3app/vue.config.js
+++ vue3app/vue.config.js
@@ -7,18 +7,29 @@ module.exports = {
       filename: `js/${projectName}.js`,
       devtoolNamespace: projectName,
     },
-    // externals: [],
+    devtool: 'source-map',
+    cache: false,
   },
-  // chainWebpack: config => {
-  //   config.externals(['vue']);
-  // },
+  chainWebpack: config => {
+    config.devServer
+      .headers({
+        "Access-Control-Allow-Origin": "*",
+      })
+
+    config.optimization.delete("splitChunks");
+    config.output.libraryTarget("umd");
+  },
 
   // In this scope, we put stuff available through Vue Configuration Reference
   devServer: {
     https: false,
     host: 'localhost',
     port,
+    allowedHosts: 'all',
   },
   filenameHashing: false,
+  css: {
+    extract: false,
+  }
 };
 
